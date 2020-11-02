8c8a5879302dd3e4ac63cd7e653fd3e550229b9e
Timber1900 <Timber.gamer@gmail.com>
2020-11-02 21:02:05 +0000

    Updated OpenTK to 4.0.6 and other nuget packages, deleted JSON and Image Sharp libraries (#7)

--- Program/MainRenderWindow.cs
+++ Program/MainRenderWindow.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
